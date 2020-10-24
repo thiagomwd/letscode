bca6c9418e0f05789dfe65923d02ac77435ad32e
Thiago Miranda <thiagomwd@gmail.com>
2020-10-24 11:01:11 -0300

    add a optimize condition nextGreatSameDigit

--- app/src/challenges/nextGreatSameDigit.js
+++ app/src/challenges/nextGreatSameDigit.js
@@ -6,19 +6,21 @@ function getNext(num) {
   let possible = false;
   let stringNumber = String(num);
   let oSplited = stringNumber.split('');
-  let splited = stringNumber.split('');
-  const greatNumber = splited.sort().reverse()[0];
-  const indexGreatNumber = stringNumber.indexOf(greatNumber);
+  if (oSplited.length > 1) {
+    let splited = stringNumber.split('');
+    const greatNumber = splited.sort().reverse()[0];
+    const indexGreatNumber = stringNumber.indexOf(greatNumber);
 
-  
-  if(indexGreatNumber > 0) {
-    possible = true;
-    let aux = oSplited[indexGreatNumber - 1];
-    oSplited[indexGreatNumber] = aux;
-    oSplited[indexGreatNumber - 1] = greatNumber
-    let as = oSplited.splice(indexGreatNumber, Number.MAX_VALUE);
-    oSplited = oSplited.concat(as.sort());
+
+    if (indexGreatNumber > 0) {
+      possible = true;
+      let aux = oSplited[indexGreatNumber - 1];
+      oSplited[indexGreatNumber] = aux;
+      oSplited[indexGreatNumber - 1] = greatNumber
+      let as = oSplited.splice(indexGreatNumber, Number.MAX_VALUE);
+      oSplited = oSplited.concat(as.sort());
+    }
   }
-    
+
   return possible ? oSplited.join('') : possible;
 }
